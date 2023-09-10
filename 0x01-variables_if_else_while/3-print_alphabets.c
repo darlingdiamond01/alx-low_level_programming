8deb7b488d89d05f7ac337c5f98a83f9fd8e4355
darlingdiamond01 <darlingmaria01@gmail.com>
2023-09-10 12:12:06 -0700

    eyinnaya

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,15 @@
 int main(void)
 
 {
-	char letter;
-	for(letter = 'a'; letter <= 'z'; letter++)
+	char low, up;
 
-	{
-		if(letter != 'e' && letter != 'q')
-			putchar(letter);
-	}
-	putchar("\n");
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+
+	for (up = 'A'; up <= 'Z'; up++)
+		putchar(up);
+
+	putchar('\n');
+
 	return (0);
 }
