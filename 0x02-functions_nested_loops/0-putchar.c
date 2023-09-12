4f6a91db922c96133e8fa0b39af539b8b71944dc
darlingdiamond01 <darlingmaria01@gmail.com>
2023-09-12 09:19:32 -0700

    eyinnaya

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * Return:n always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('e');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
