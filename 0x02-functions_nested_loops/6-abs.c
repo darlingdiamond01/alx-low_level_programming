eb6685509e7c1d29ef77d93c5ddfe4e527bc0963
darlingdiamond01 <darlingmaria01@gmail.com>
2023-09-13 15:37:15 -0700

    eyinnaya

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of the integer
+ * @n: the int to be checked
+ * Return: the value of int
+ */
+
+int _abs(int n)
+{
+	return (n < 0 ? n * -1 : n);
+}
