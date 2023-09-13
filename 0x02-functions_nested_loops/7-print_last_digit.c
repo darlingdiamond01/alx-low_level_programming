9d0a8409de8c5c0af23f05e32cc29c6d078d0162
darlingdiamond01 <darlingmaria01@gmail.com>
2023-09-13 15:45:48 -0700

    eyinnaya

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,16 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last;
+	
+	last = n % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
 
-	if (n < 0)
-		last_digit = -1;
+	_putchar(last + '0');
 
-	_putchar(last_digit + '0');
-
-	return (last_digit);
+	return (last);
 }
 
