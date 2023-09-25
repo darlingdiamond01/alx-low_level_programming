0df6a74c1d3bf627956169ed2f7ab2b6e58f6400
darlingdiamond01 <darlingmaria01@gmail.com>
2023-09-25 12:48:47 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-main.c
+++ 0x07-pointers_arrays_strings/3-main.c
@@ -1,31 +1,18 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _strspn - Function that gets the length of a prefix substring
- * @s: pointer to string s
- * @accept: This is the second string
- * Return: number of bytes in the initial segment of s which consist only of
- * bytes from accept
+ * main - check the code
+ *
+ * Return: Always 0.
  */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
 
-unsigned int _strspn(char *s, char *accept)
-{
-unsigned int i, j, counter;
-
-counter = 0;
-for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; accept[j] != '\0'; j++)
-{
-if (accept[j] == s[i])
-{
-counter++;
-break;
-}
-}
-if (accept[j] != s[i])
-{
-break;
-}
-}
-return (counter);
+    n = _strspn(s, f);
+    printf("%u\n", n);
+    return (0);
 }
